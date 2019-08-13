0510d3348d69fb5abfb233cc377a216a2366bf84
positivecse <47621096+positivecse@users.noreply.github.com>
2019-08-13 21:37:24 +0530

    Update Armstrong.c

--- Armstrong.c
+++ Armstrong.c
@@ -1,33 +1,26 @@
 void main()
 {
-int a,r,sum=0,t,n=0;
+int a,r,sum=0,t;
 clrscr();
 printf("Enter any number:");
 scanf("%d",&a);
+t=a;
 if(a<=100000)
 {
-t=a;
-while(a!=0)
-{
-a=a/10;
-++n;
-}
-t=a;
 while(a>0)
 {
 r=a%10;
-sum=sum+pow(r,n);
+sum=sum+r*r*r;
 a=a/10;
 }
-t=a;
-if(a==sum)
+if(t==sum)
 printf("Armstrong number");
 else
 printf("Not a Armstrong number");
 }
 else
 {
-printf("Out of Rangr");
+printf("Out of Range");
 }
 getch();
 }
